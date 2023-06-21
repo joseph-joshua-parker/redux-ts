17f2273d772fa25800521200b315f5edc4730123
joseph-joshua-parker <joelnashjobs@yahoo.com>
2023-06-21 17:25:44 -0300

    Lesson 328: RepositoriesList component: initial setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import { Provider } from "react";
 import {store} from '../state';
+import RepositoriesList from './RepositoriesList';
 
 const App = () =>{
 	return <Provider store={store}>
 		<h1>Hi!</h1>
+		<RepositoriesList/>
 	</Provider>
 }
 
